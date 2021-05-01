e0594b5ed5f4f0bcf453d37923a8e09c1f11cf93
SpaceFace02 <73184157+SpaceFace02@users.noreply.github.com>
2021-05-02 00:50:31 +0530

    Volunteer Info

---
+++ src/Layout/Main.js
@@ -0,0 +1,103 @@
+import React from "react";
+import "./style.css";
+
+const Main = () => {
+  return (
+    <>
+      <main id="main">
+        <section id="about" class="about">
+          <div class="container">
+            <div class="section-title" data-aos="zoom-out">
+              <h2>JOIN</h2>
+              <p>How to join</p>
+            </div>
+
+            <div class="row content" data-aos="fade-up">
+              <div class="col-lg-6">
+                <p>To join, we need you to:</p>
+                <ul>
+                  <li>
+                    <i class="ri-check-double-line"></i> Live in India and be
+                    over the age of 18.
+                  </li>
+                  <li>
+                    <i class="ri-check-double-line"></i> Have an internet
+                    connection. This is because tasks will be sent directly to
+                    your device through the EK website. Don’t worry if you are
+                    not familiar with this technology, there is plenty of help
+                    available.
+                  </li>
+                  <li>
+                    <i class="ri-check-double-line"></i> Decide which of the EK
+                    Volunteer Responder role/s you would like to do.
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0">
+                <ul>
+                  <li>
+                    <i class="ri-check-double-line"></i> Consider your own
+                    health condition when choosing role/s. If you are clinically
+                    vulnerable to coronavirus (COVID-19) you may prefer our
+                    telephone-based Check In and Chat roles.{" "}
+                  </li>
+                  <li>
+                    <i class="ri-check-double-line"></i>Be able to volunteer and
+                    go on duty as much as you possibly can.
+                  </li>
+                </ul>
+                <a href="/" class="btn-learn-more">
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="features" class="features">
+          <div class="container">
+            <ul class="nav nav-tabs row d-flex">
+              <li class="nav-item col-3" data-aos="zoom-in">
+                <a class="nav-link show" data-bs-toggle="tab" href="#tab-1">
+                  {/* <i class="ri-gps-line"></i> */}
+                  <h4 class="d-none d-lg-block">How the program works?</h4>
+                </a>
+              </li>
+              <li
+                class="nav-item col-3"
+                data-aos="zoom-in"
+                data-aos-delay="100"
+              >
+                <a class="nav-link show" data-bs-toggle="tab" href="#tab-2">
+                  <h4 class="d-none d-lg-block">Volunteer Roles</h4>
+                </a>
+              </li>
+              <li
+                class="nav-item col-3"
+                data-aos="zoom-in"
+                data-aos-delay="200"
+              >
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-3">
+                  <h4 class="d-none d-lg-block">
+                    Who's life you will be brightening
+                  </h4>
+                </a>
+              </li>
+              <li
+                class="nav-item col-3"
+                data-aos="zoom-in"
+                data-aos-delay="300"
+              >
+                <a class="nav-link" data-bs-toggle="tab" href="#tab-4">
+                  <h4 class="d-none d-lg-block">Offline Options</h4>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Main;
